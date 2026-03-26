ac801383294a227907442f1b6a7cf8b5b80272ee
Seungkyu-Han <trust1204@gmail.com>
2026-03-26 23:46:58 +0900

    update: modify Jest coverage reporters configuration

--- jest.config.ts
+++ jest.config.ts
@@ -25,7 +25,7 @@ const config: Config = {
         },
     },
 
-    coverageReporters: ['text', 'lcov', 'html'],
+    coverageReporters: ['text', 'text-summary'],
 
     transform: {
         '^.+\\.ts$': 'ts-jest',
