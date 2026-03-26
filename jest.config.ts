1a32adfed626c4ee92b33943a26cd212b5f19ac7
Seungkyu-Han <trust1204@gmail.com>
2026-03-26 23:20:23 +0900

    feat: add Jest configuration and scripts for testing

---
+++ jest.config.ts
@@ -0,0 +1,39 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+
+    testMatch: ['**/?(*.)+(spec|test).ts'],
+
+    collectCoverage: true,
+    collectCoverageFrom: [
+        'src/**/*.ts',
+        '!src/**/*.module.ts',
+        '!src/main.ts',
+        '!**/*.d.ts',
+    ],
+
+    coverageDirectory: 'coverage',
+
+    coverageThreshold: {
+        global: {
+            branches: 80,
+            functions: 85,
+            lines: 85,
+            statements: 85,
+        },
+    },
+
+    coverageReporters: ['text', 'lcov', 'html'],
+
+    transform: {
+        '^.+\\.ts$': 'ts-jest',
+    },
+
+    moduleFileExtensions: ['ts', 'js', 'json'],
+
+    verbose: true,
+};
+
+export default config;
