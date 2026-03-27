a76bfbb6159b7518875c95306b15258fff114e93
Seungkyu-Han <trust1204@gmail.com>
2026-03-27 23:31:46 +0900

    test: add unit tests for `ERROR_REPORTER_OPTIONS`

---
+++ src/nest/error-reporter.token.spec.ts
@@ -0,0 +1,7 @@
+import { ERROR_REPORTER_OPTIONS } from './error-reporter.tokens';
+
+describe('ERROR_REPORTER_OPTIONS', () => {
+    it('should be defined', () => {
+        expect(ERROR_REPORTER_OPTIONS).toBeDefined();
+    });
+});
