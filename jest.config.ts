1d9ac08961fd0c5edf4695368563a7a965ed499f
Seungkyu-Han <trust1204@gmail.com>
2026-03-28 00:07:57 +0900

    update: integrate Codecov and adjust Jest coverage configuration

--- jest.config.ts
+++ jest.config.ts
@@ -25,7 +25,7 @@ const config: Config = {
         },
     },
 
-    coverageReporters: ['text', 'text-summary'],
+    coverageReporters: ["text", "lcov"],
     coveragePathIgnorePatterns: [
         "index.ts"
     ],
